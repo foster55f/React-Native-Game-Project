4dd94c86aca0033253b4491a28a34cf7c71e0381
“Foster <fosterataylor@yahoo.com>
2020-03-30 15:44:35 -0600

    Add update

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
 
 
 
